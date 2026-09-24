ef92f793ad370ffaaa838ec8387663228881f4f3
HarborHaus LLC <76707290+thomasny9@users.noreply.github.com>
2026-09-24 12:54:45 -0400

    Use Nuvo product name in config entry

--- custom_components/sonos_remote/config_flow.py
+++ custom_components/sonos_remote/config_flow.py
@@ -14,7 +14,7 @@ class SonosRemoteConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         self._abort_if_unique_id_configured()
 
         if user_input is not None:
-            return self.async_create_entry(title="Sonos Remote", data={})
+            return self.async_create_entry(title="Sonos Remote with Nuvo", data={})
 
         return self.async_show_form(
             step_id="user",
